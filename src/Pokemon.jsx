b3454bcf15697cf356778b11caf86d5278e595f2
Shreejan <shreejanid123@gmail.com>
2025-01-27 20:43:07 +0545

    data fetching done

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,12 +1,53 @@
-
+import React, { useEffect } from 'react';
+import './index.css';
 
 export const Pokemon = () => {
 
+    const [pokemon, setPokemon] = useState([]);
+
+    const API = " https://pokeapi.co/api/v2/pokemon?limit=24";
+
+const fetchPokemon = async () => {
+    try{
+const response = await fetch(API)
+const data = await response.json()
+// console.log(data);
+const DetailedPokemonData = data.results.map(async(curPokemon)=>{
+// console.log(curPokemon.url); 
+const res = await fetch(curPokemon.url);
+const data = await res.json();
+
+// console.log(data);
+
+return data;
+
+})
+const detailedResponses = await Promise.all(DetailedPokemonData);
+setPokemon(detailedResponses);
+    }
+    catch(error){
+console.log(error);
+    }
+}
+
+useEffect(() => {
+fetchPokemon();
+
+},[])
+
+
 return (
 
-  <div>
-    <h1>Hello i am Pokemon</h1>
-  </div>
+  <>
+  <section className="container">
+<header>
+<h1>Let's Catch Pokemon</h1>
+</header>
+
+  </section>
+    
+    
+  </>
 
 )
 
