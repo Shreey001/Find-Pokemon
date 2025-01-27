9dc29a7ab88092f4ff6816a38f76de93502e0fb0
Shreejan <shreejanid123@gmail.com>
2025-01-27 21:07:46 +0545

    fixed state import error

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
 import { PokemonCards } from './PokemonCards.jsx';
 export const Pokemon = () => {
